65bb11828e0da313e0f4c9a91aa0974aa8b8e90d
Srujan Deshpande <srujan.deshpande@gmail.com>
2020-04-05 20:40:20 +0530

    Cleanup and bugs

--- static/js/orgdash.js
+++ static/js/orgdash.js
@@ -7,10 +7,23 @@ $(function(){
       async: true,
       success:function(msg){
         var count = msg['count'];
-        $('#alertcount').html(count);
         var r = "record";
         for (var i=0;i<count;i++){
           $("#alertTable").append("<tr><td class='body-item mbr-fonts-style display-7'>"+msg[r+i]['type']+"</td><td class='body-item mbr-fonts-style display-7'>"+msg[r+i]['location']+"</td><td class='body-item mbr-fonts-style display-7'>"+msg[r+i]['priority']+"</td><td class='body-item mbr-fonts-style display-7'>"+msg[r+i]['phone']+"</td></tr>");
+        }
+      }
+  });
+
+  $.ajax({
+      url:'/get_drive',
+      type:'post',
+      data:"",
+      async: true,
+      success:function(msg){
+        var count = msg['count'];
+        var r = "record";
+        for (var i=0;i<count;i++){
+          $("#driveTable").append("<tr><td class='body-item mbr-fonts-style display-7'>"+msg[r+i]['name']+"</td><td class='body-item mbr-fonts-style display-7'>"+msg[r+i]['phone']+"</td><td class='body-item mbr-fonts-style display-7'>"+msg[r+i]['location']+"</td></tr>");
         }
       }
   });
